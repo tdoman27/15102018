151ff1f7cf2f77786233b8959c3ba336604e86bb
tdoman27 <44172925+tdoman27@users.noreply.github.com>
2018-10-15 19:26:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 function zmienKolor()
 {
     this.className= "nowa";
@@ -10,16 +8,36 @@ function zmienKolor2 ()
     this.className=""
 }
 
-function alert ()
+function spr ()
 {
-    alert("imie")
+	var wiek=document.getElementById("wiek").value;
+	var imie=document.getElementById("imie").value;
+	var ul = document.getElementById("lista");
+	var li = document.createElement("li");
+	
+	
+	if(wiek<18){
+		alert(imie+", musisz mieć 18 lat, aby wpisać się na listę!!");
+		document.getElementById("wiek").value="";
+		document.getElementById("imie").value="";
+	}
+	
+	else{
+		li.appendChild(document.createTextNode(imie));
+		ul.appendChild(li);
+		document.getElementById("wiek").value="";
+		document.getElementById("imie").value="";
+	}
+
 }
 
-var imie=document.getElementById("imie")
-var test=document.getElementById("kolorowe");
+
+
+var test=document.getElementById("naglowek");
 var przycisk=document.getElementById("przycisk")
+
 
 test.onmouseover = zmienKolor;
 test.onmouseout = zmienKolor2;
-przycisk.onclick=alert;
+przycisk.onclick = spr;
 
